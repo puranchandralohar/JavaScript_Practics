bbcf92d80960ecb128a63e92f29d092483eb3d82
puranchandralohar <clpuran95@gmail.com>
2022-06-17 19:55:46 +0530

    While loop added

--- evenno.js
+++ evenno.js
@@ -1,9 +1,16 @@
 let n = parseInt(process.argv[2]);
 
-for(let i=1;i<=n;i++){
-    if(i%2==0){
-        console.log(i);  
-    }
+// for(let i=1;i<=n;i++){
+//     if(i%2==0){
+//         console.log(i);  
+//     }
    
-}
+// }
 
+let i= 1;
+while(i<=n){
+    if(i%2==0){
+        console.log(i);
+    }
+    i++;
+}
