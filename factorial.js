b1445f67449f24533cbb90c7b9537a9e6af7970f
puranchandralohar <clpuran95@gmail.com>
2022-06-17 20:18:08 +0530

    Factorial while loop

--- factorial.js
+++ factorial.js
@@ -3,9 +3,14 @@ let a = parseInt(process.argv[2]);
 
 let fact = 1;
 
-for(i=a;i>=1;i--){
-  fact= fact*i
-  console.log(fact);
+// for(i=a;i>=1;i--){
+//   fact= fact*i
+//   console.log(fact);
+// }
+
+let i=a;
+while(i>=1){
+    fact=fact*i;
+    console.log(fact);
+    i--
 }
-
-
