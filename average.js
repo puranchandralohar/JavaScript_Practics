2f4ab18b0cfb9955aea4f5e62a10d56b327cb4fe
puranchandralohar <clpuran95@gmail.com>
2022-06-17 17:17:52 +0530

    added average

---
+++ average.js
@@ -0,0 +1,13 @@
+// Find average
+
+let number1= parseInt(process.argv[2]);
+let number2 = parseInt(process.argv[3]);
+let number3 = parseInt(process.argv[4]);
+let number4 = parseInt(process.argv[5]);
+let number5 = parseInt(process.argv[6]);
+
+let sum = number1+number2+number3+number4+number5;
+
+let average = sum /5;
+
+console.log("Average = "+average);
