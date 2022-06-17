eb94fbd9d62aadcecde1aa59b9c5d798ee7c2761
puranchandralohar <clpuran95@gmail.com>
2022-06-17 19:52:56 +0530

    While loop added

--- natural.js
+++ natural.js
@@ -1,6 +1,13 @@
 //Print Natural Numbers upto n...
 
 let n = parseInt(process.argv[2]);
-for(i=0;i<=n;i++){
-    console.log(i);
+// for(i=0;i<=n;i++){
+//     console.log(i);
+// }
+
+// while loop
+let i =0;
+while(i<=n){
+    console.log(i)
+    i++;
 }
