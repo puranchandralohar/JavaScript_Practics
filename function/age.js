d1bc320598c1f67eeed9dcc2f9f78d93c9d315ca
puranchandralohar <clpuran95@gmail.com>
2022-06-21 17:40:01 +0530

    arrow function

---
+++ function/age.js
@@ -0,0 +1,8 @@
+// function that takes the age in years and returns the age in days.
+
+const ageInDays = (ageInYear) =>{
+    return ageInYear*365;
+
+}
+
+console.log(ageInDays(22));
---
+++ function/mintosec.js
@@ -0,0 +1,7 @@
+// convert minute to second
+
+let second = (minute) => {
+
+    return minute*60;
+}
+console.log(second(5));
