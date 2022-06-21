f1bd86ee696e2c6907279c3a99cc4d0513b0d437
puranchandralohar <clpuran95@gmail.com>
2022-06-21 17:19:05 +0530

    Arrow Function

---
+++ function/biggestnumber.js
@@ -0,0 +1,8 @@
+let biggest = (number1,number2) =>{
+    if(number1>number2){
+        return "Number1 is greater";
+    }else{
+        return "Number2 is greater";
+    }
+}
+console.log(biggest(10,15));
