e5ba9ed1a7e9fcb0e3f87b1ac8256b72945f2ada
puranchandralohar <clpuran95@gmail.com>
2022-06-23 17:31:40 +0530

    Annoynomus IIFE Arrow function

---
+++ callback function/printmsg.js
@@ -0,0 +1,15 @@
+
+// Arrow Function
+let msg = (message)=>{
+    console.log(message);
+
+};
+msg("Hello World!");
+
+
+
+//Annoynomus Arrow IIFE Function
+// ((message) => {
+//     console.log(message);
+
+// })("Hello world!");
