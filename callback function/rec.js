8f805c60326605b3f779803bb7e511ed0192cdfd
puranchandralohar <clpuran95@gmail.com>
2022-06-23 17:30:15 +0530

    Recurssion

--- callback function/rec.js
+++ callback function/rec.js
@@ -1,10 +1,23 @@
+// function fact(num,facto){
+//     if(num>0){
+//         facto=facto*num;
+//         // console.log(num);
+//         // console.log(facto);
+//         return fact(num-1,facto);       
+//     }
+//     return facto;
+// }
+// console.log(fact(5,1));
+
+
 function fact(num,facto){
     if(num>0){
-        facto=facto*num;
+        facto=num*facto;
         // console.log(num);
-        // console.log(facto);
-        return fact(num-1,facto);       
+        return fact(num-1,facto);
     }
     return facto;
 }
+console.log(fact(4,1));
 console.log(fact(5,1));
+console.log(fact(6,1));
