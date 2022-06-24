553dadea3d7bd7764832e4fe72f6230339ad6f5b
puranchandralohar <clpuran95@gmail.com>
2022-06-24 11:32:28 +0530

    Even no using Arrow function

---
+++ function/even.js
@@ -0,0 +1,14 @@
+let getEven = (a,b) => {
+    for(let i=a ; i<=b; i++){
+        if(i%2==0){
+            console.log(i);
+        }
+    }
+    // for (let i=a;i>=b;i--){
+    //     if(i%2==0){
+    //         console.log(i);
+    //     }
+    // }
+}
+// getEven(20,10);
+getEven(10,20);
